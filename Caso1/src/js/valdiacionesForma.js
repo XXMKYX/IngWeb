d8691a9e544875fddaab66bdff0a6ed7c4318d76
elJamesSnz <72090281+elJamesSnz@users.noreply.github.com>
2020-11-16 16:38:03 -0600

    Cambios Masivos
    - Se cambió el index a raiz
    - Se cambiaron nombres de campos
    - Se agregó option maestría/doctorado

---
+++ Caso1/src/js/valdiacionesForma.js
@@ -0,0 +1,23 @@
+function cambiarForma(input) {
+    var valor = input.value;
+    console.log(valor);
+    if(valor == 1)
+    {
+        var forma = document.getElementById("formaM");
+        var forma2 = document.getElementById("formaD");
+        forma.style.display = "block";
+        forma2.style.display = "none";
+    }
+    else if(valor == 2){
+        var forma = document.getElementById("formaM");
+        var forma2 = document.getElementById("formaD");
+        forma.style.display = "none";
+        forma2.style.display = "block";
+    }
+    else{
+        var forma = document.getElementById("formaM");
+        var forma2 = document.getElementById("formaD");
+        forma.style.display = "none";
+        forma2.style.display = "none";
+    }
+}
