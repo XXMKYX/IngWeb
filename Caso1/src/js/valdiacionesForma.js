3db8e283fe815f0977ad8aaddc5019d1a26e34bb
elJamesSnz <72090281+elJamesSnz@users.noreply.github.com>
2020-12-19 01:59:17 -0600

    menus
    agregué submenu

--- Caso1/src/js/valdiacionesForma.js
+++ Caso1/src/js/valdiacionesForma.js
@@ -18,10 +18,31 @@ for (i = 0; i < tablinks.length; i++) {
 document.getElementById(cityName).style.display = "block";
 evt.currentTarget.className += " active";
 }
+
+function changeTab2(evt, cityName) {
+    // Declare all variables
+    var i, tabcontent, tablinks;
+    
+    // Get all elements with class="tabcontent" and hide them
+    tabcontent = document.getElementsByClassName("tabcontent2");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    
+    // Get all elements with class="tablinks" and remove the class "active"
+    tablinks = document.getElementsByClassName("tablinks2");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+    
+    // Show the current tab, and add an "active" class to the button that opened the tab
+    document.getElementById(cityName).style.display = "block";
+    evt.currentTarget.className += " active";
+    }
+
   
 function cambiarForma(input) {
     var valor = input.value;
-    console.log(valor);
     if(valor == 1)
     {
         var forma = document.getElementById("formaM");
