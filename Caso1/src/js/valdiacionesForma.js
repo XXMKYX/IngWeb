da89af3c601b2486fafde780fbfc078fc60362d7
elJamesSnz <72090281+elJamesSnz@users.noreply.github.com>
2021-01-19 00:44:20 -0600

    Unión Formas
    Se hizo la unión de datos personales y académicos.
    
    Se hizo la separación mediante radio button de maestría y doctorado.
    
    Las tabs muestran los datos académicos y personales.

--- Caso1/src/js/valdiacionesForma.js
+++ Caso1/src/js/valdiacionesForma.js
@@ -22,6 +22,38 @@ var handler = function(input) {
     }
 };
 
+var HideTab = function(input) {
+    var MaestriaDatosPM = document.getElementById('nav-datos');
+    var MaestriaDatosAM = document.getElementById('nav-informacion');
+
+    var DoctoradoDatosPD = document.getElementById('nav-datosD');
+    var DoctoradoDatosAD = document.getElementById('nav-informacionD');
+   // var archivosMaestria = document.getElementById('archivosMaestria');
+    //var archivosDoctorado = document.getElementById('archivosDoctorado');
+    //var Forma = document.getElementById('formaM');
+    var valor = input.value;
+    
+    //1 si es Maestría, 2 si es Doctorado
+    if(valor == 1){
+        MaestriaDatosPM.style.display = "block";        
+        MaestriaDatosAM.style.display = "none";
+    }else if(valor == 2){
+        MaestriaDatosPM.style.display = "none";
+        MaestriaDatosAM.style.display = "block";        
+    }
+    else{
+        if(valor == 3){
+            DoctoradoDatosPD.style.display = "block";        
+            DoctoradoDatosAD.style.display = "none";
+        }
+        else if(valor == 4){
+            DoctoradoDatosPD.style.display = "none";
+            DoctoradoDatosAD.style.display = "block";        
+        }
+    }
+};
+
+
 function changeTab(evt, Tabame) {
 // Declare all variables
 var i, tabcontent, tablinks;
