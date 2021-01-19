77a309a796db6d26c09a7a51aa45e5cf1ee41e67
Miguel Angel <51009414+XXMKYX@users.noreply.github.com>
2021-01-19 01:51:58 -0600

    Unificacion Final Maestria
    Formulario Maestria Completado!

--- Caso1/src/routes/index.js
+++ Caso1/src/routes/index.js
@@ -96,7 +96,7 @@ router.get('/personal_data',(req,res)=>{
 router.post('/personal_data',(req,res)=>{
   console.log(req.body);
   
-  const { nombreM, apaternoM, amaternoM, fechaM, lugarM, nacionalidadM, civilM, curpM, dependenciasM, telefonoM, emailM, skypeM, fbM, CalleM, NoExtM, NoIntM, ColoniaM, CiudadM, EstadoM, CPM} = req.body;
+  const { nombreM, apaternoM, amaternoM, fechaM, lugarM, nacionalidadM, civilM, curpM, dependenciasM, telefonoM, emailM, skypeM, fbM, CalleM, NoExtM, NoIntM, ColoniaM, CiudadM, EstadoM, CPM, InstitucionM, pinstitucionM, tituladoMaestriaM,carreraM, xpPM, xpDM, propedeuticoM, aniospropM, motivoM,archivosubido1M} = req.body;
 /* Validacion */
   //if (!nombre || !apaterno || !amaterno || !fecha|| !lugar|| !nacionalidad ||!civil ||!curp ||!dependencias ||!telefono ||!email ||!skype ||!fb ||!Calle ||!NoExt ||!NoInt ||!Colonia ||!Ciudad ||!Estado ||!CP) {
   //  res.status(400).send("Tienes que llenar todos los datos - Personal");
@@ -123,7 +123,17 @@ router.post('/personal_data',(req,res)=>{
     ColoniaM,
     CiudadM,
     EstadoM,
-    CPM
+    CPM,
+    InstitucionM,
+    pinstitucionM,
+    tituladoMaestriaM,
+    carreraM,
+    xpPM,
+    xpDM,
+    propedeuticoM,
+    aniospropM,
+    motivoM,
+    archivosubido1M
   };
 
   // agregando al array
